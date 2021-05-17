3031d29fe2dba9f45fa247418f1e092756cd62a7
Nelson <nelson.ortega@microfocus.com>
2021-05-17 15:14:07 -0600

    Router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import Home from './pages/Home'
+import Inbox from './pages/Inbox'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import Explore from './pages/Explore'
+import Settings from './pages/Settings'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>Instagram Clone</div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/inbox" component={Inbox} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/explore" component={Explore} />
+      <Route path="/settings" component={Settings} />
+    </Router>
   )
 }
 
